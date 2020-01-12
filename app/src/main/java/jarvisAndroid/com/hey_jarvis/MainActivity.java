decb4e11c7de7cf7dc9934d4ecfa3813a97dc900
김준성 <anima94@naver.com>
2020-01-12 14:19:45 +0900

    summary

--- app/src/main/java/jarvisAndroid/com/hey_jarvis/MainActivity.java
+++ app/src/main/java/jarvisAndroid/com/hey_jarvis/MainActivity.java
@@ -1,32 +1,19 @@
 package jarvisAndroid.com.hey_jarvis;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.actions.ReserveIntents;
-
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -55,7 +42,14 @@ public class MainActivity extends AppCompatActivity {
                     textView.setText("");
                     check=true;
                 }
-                inputVoice(textView);
+                Intent intent =new Intent (RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.KOREA);
+                try{
+                    startActivityForResult(intent,200);
+                }catch(ActivityNotFoundException a){
+                    Toast.makeText(getApplicationContext(),"intent pool",Toast.LENGTH_SHORT).show();
+                }
             }
         });
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
@@ -65,64 +59,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void inputVoice(final TextView txt) {
-        try {
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
-            final SpeechRecognizer stt = SpeechRecognizer.createSpeechRecognizer(this);
-            stt.setRecognitionListener(new RecognitionListener() {
-                @Override
-                public void onReadyForSpeech(Bundle params) {
-                    toast("음성 입력 시작...");
-                }
 
-                @Override
-                public void onBeginningOfSpeech() {
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==200){
+            if(resultCode==RESULT_OK && data!=null){
+                ArrayList<String> result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                textView.append("[나] "+result.get(0)+"\n");
+                replyAnswer(result.get(0), textView);
 
-                }
-
-                @Override
-                public void onRmsChanged(float rmsdB) {
-
-                }
-
-                @Override
-                public void onBufferReceived(byte[] buffer) {
-                }
-
-                @Override
-                public void onEndOfSpeech() {
-                    toast("음성 입력 종료");
-                }
-
-                @Override
-                public void onError(int error) {
-                    toast("오류 발생 : " + error);
-                    stt.destroy();
-                }
-
-                @Override
-                public void onResults(Bundle results) {
-                    ArrayList<String> result = (ArrayList<String>) results.get(SpeechRecognizer.RESULTS_RECOGNITION);
-                    txt.append("[나] "+result.get(0)+"\n");
-                    replyAnswer(result.get(0), txt);
-                    stt.destroy();
-                }
-
-                @Override
-                public void onPartialResults(Bundle partialResults) {
-
-                }
-
-                @Override
-                public void onEvent(int eventType, Bundle params) {
-
-                }
-            });
-            stt.startListening(intent);
-        } catch (Exception e) {
-            toast(e.toString());
+            }
         }
     }
 
