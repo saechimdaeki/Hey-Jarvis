3dfb5e76c0abc706587a6505d3fc8e778e783c94
saechimdaeki <anima94@naver.com>
2020-01-20 23:49:58 +0900

    (feat) MusicPlayer.java (service)
    update readme

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 ### Develop progress
 #### [개발진행상황](./DevelopNote)
 
+## 개발시 어려운점
+
+- android가 oreo이상버전시 notification이 channelID를 쓰고 지정해야한다는것. 이것을 몰랐었다(시간매우소요됨.)
+- 또한 notification클릭이 되지를 않고있는상황 (클릭했을때 앱이켜지게 하려고함 ) 
+
 
 ## 🤝 How to Contribute
 1.  Fork it
@@ -20,6 +25,7 @@
  * Alarms Alignment Function
  * dialing function
  * camera function 
+ * notification click event 
 * 택시 불러 줘 라고말할시 
    callCar() 호출  (This function is only available on wear os..?)
 <code>
---
+++ app/src/main/java/jarvisAndroid/com/hey_jarvis/MusicPlayer.java
@@ -0,0 +1,120 @@
+package jarvisAndroid.com.hey_jarvis;
+
+import android.app.Notification; //foreground
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+import android.database.Cursor;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+public class MusicPlayer extends Service {
+
+    MediaPlayer media;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String id = "my_channel_01";
+        CharSequence name = "test";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel mChannel = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            mChannel = new NotificationChannel(id, name, importance);
+        }
+        mChannel.enableLights(true);
+        mChannel.setLightColor(Color.RED);
+        mNotificationManager.createNotificationChannel(mChannel);
+        int notifyID = 1;
+        String CHANNEL_ID = "my_channel_01";
+        Notification.Builder notice=new Notification.Builder(this);
+        notice.setSmallIcon(R.drawable.jarvisicon); //https://makeappicon.com/
+        notice.setContentTitle("jarvis뮤직");
+        notice.setChannelId(CHANNEL_ID);
+        notice.setContentText("뮤직플레이어 가동중");
+        notice.build();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            startForeground(1,notice.build());
+            try{
+                String[] musics=audio();
+                media=new MediaPlayer();
+                media.reset();
+                int rand=(int)Math.floor(Math.random()*musics.length);
+                media.setDataSource(musics[rand]);
+                media.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        try {
+                            mediaPlayer.reset();
+                            String[] music=audio();
+                            int rand2=(int)Math.floor(Math.random()*music.length);
+                            mediaPlayer.setDataSource(music[rand2]);
+                            mediaPlayer.prepare();
+                            mediaPlayer.start();
+                        }catch(Exception e){
+                            tst(e.toString());
+                        }
+                    }
+                });
+
+
+                media.prepare();
+                media.start();
+            }catch(Exception e){
+                tst(e.toString());
+            }
+        }// require >= JELLYBEAN
+        return START_NOT_STICKY;
+    }
+
+    public String[] audio(){
+        String[] ret=null;
+        try{
+            Cursor cursor=getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{MediaStore.Audio.Media.DATA},null,null,null);
+            if(cursor.getCount()==0){
+                return null;
+            }
+            ret=new String[cursor.getCount()];
+            cursor.moveToFirst();
+            ret[cursor.getPosition()]=cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+            while(cursor.moveToNext()){
+                ret[cursor.getPosition()]=cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+            }
+            cursor.close();
+        }catch(Exception e){
+            tst(e.toString());
+        }
+        return ret;
+    }
+    private  void tst(String toast){
+        Toast.makeText(this,toast,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 서비스가 종료될 때 실행
+        media.reset();
+       // media.stop(); // 음악 종료
+    }
+
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
