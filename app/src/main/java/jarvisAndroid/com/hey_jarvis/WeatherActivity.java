fd91ca748333cc2de2153f592121f93ace1c536f
김준성 <anima94@naver.com>
2020-05-13 15:33:41 +0900

    FIX) Location Weather :wrench:

--- app/src/main/java/jarvisAndroid/com/hey_jarvis/WeatherActivity.java
+++ app/src/main/java/jarvisAndroid/com/hey_jarvis/WeatherActivity.java
@@ -1,5 +1,6 @@
 package jarvisAndroid.com.hey_jarvis;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,12 +12,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.kwabenaberko.openweathermaplib.constants.Lang;
 import com.kwabenaberko.openweathermaplib.constants.Units;
 import com.kwabenaberko.openweathermaplib.implementation.OpenWeatherMapHelper;
@@ -25,93 +36,102 @@ import com.kwabenaberko.openweathermaplib.implementation.callbacks.ThreeHourFore
 import com.kwabenaberko.openweathermaplib.models.currentweather.CurrentWeather;
 import com.kwabenaberko.openweathermaplib.models.threehourforecast.ThreeHourForecast;
 
-public class WeatherActivity extends AppCompatActivity {
-    public String provider ;
-    public double longitude ;
-    public double latitude;
-    public double altitude ;
-    public static final String TAG = WeatherActivity.class.getSimpleName();
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WeatherActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    TextView content;
+
+
+    GoogleApiClient apiClient;
+    FusedLocationProviderClient providerClient;
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather);
-        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        /*
-        if ( Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions( WeatherActivity.this, new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                    0 );
+
+
+        content=findViewById(R.id.content);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
         }
-        else{
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            String provider = location.getProvider();
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
-            double altitude = location.getAltitude();
+        apiClient=new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        providerClient=LocationServices.getFusedLocationProviderClient(this);
 
-
-
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    1000,
-                    1,
-                    gpsLocationListener);
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                    1000,
-                    1,
-                    gpsLocationListener);
-        }
-        */
-        OpenWeatherMapHelper helper = new OpenWeatherMapHelper("4716c24d34633a1fb730d59f9519d65e"); //junseong apikey
-
-        final TextView text = findViewById(R.id.weathertext);
-
-        helper.setUnits(Units.IMPERIAL);
-
-        helper.setLang(Lang.KOREAN);
-
-
-        helper.getThreeHourForecastByGeoCoordinates(36, 127, new ThreeHourForecastCallback() {
-            @Override
-            public void onSuccess(ThreeHourForecast threeHourForecast) {
-                text.setText("City/Country: " + threeHourForecast.getCity().getName() + "/" + threeHourForecast.getCity().getCountry() + "\n"
-                        + "Forecast Array Count: " + threeHourForecast.getCnt() + "\n"
-                        //For this example, we are logging details of only the first forecast object in the forecasts array
-                        + "First Forecast Date Timestamp: " + threeHourForecast.getList().get(0).getDt() + "\n"
-                        + "First Forecast Weather Description: " + threeHourForecast.getList().get(0).getWeatherArray().get(0).getDescription() + "\n"
-                        + "First Forecast Max Temperature: " + threeHourForecast.getList().get(0).getMain().getTempMax() + "\n"
-                        + "First Forecast Wind Speed: " + threeHourForecast.getList().get(0).getWind().getSpeed() + "\n"
-                );
-            }
-
-            @Override
-            public void onFailure(Throwable throwable) {
-                Log.v(TAG, throwable.getMessage());
-            }
-        });
     }
-    /*
-    final LocationListener gpsLocationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
 
-            String provider = location.getProvider();
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
-            double altitude = location.getAltitude();
+    private void showToast(String message){
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    private void setLocationInfo(Location location){
+        if(location != null){
 
+            OpenWeatherMapHelper helper = new OpenWeatherMapHelper("4716c24d34633a1fb730d59f9519d65e"); //junseong apikey
 
+            helper.setUnits(Units.IMPERIAL);
+            helper.setLang(Lang.KOREAN);
+            Log.v("latitude",String.valueOf(location.getLatitude()));
+            helper.getThreeHourForecastByGeoCoordinates(Double.parseDouble(String.valueOf(location.getLatitude())), Double.parseDouble(String.valueOf(location.getLongitude())), new ThreeHourForecastCallback() {
+                @Override
+                public void onSuccess(ThreeHourForecast threeHourForecast) {
+                    content.setText("City/Country: " + threeHourForecast.getCity().getName() + "/" + threeHourForecast.getCity().getCountry() + "\n"
+                            + "Forecast Array Count: " + threeHourForecast.getCnt() + "\n"
+                            //For this example, we are logging details of only the first forecast object in the forecasts array
+                            + "First Forecast Date Timestamp: " + threeHourForecast.getList().get(0).getDt() + "\n"
+                            + "First Forecast Weather Description: " + threeHourForecast.getList().get(0).getWeatherArray().get(0).getDescription() + "\n"
+                            + "First Forecast Max Temperature: " + threeHourForecast.getList().get(0).getMain().getTempMax() + "\n"
+                            + "First Forecast Wind Speed: " + threeHourForecast.getList().get(0).getWind().getSpeed() + "\n"
+                    );
+                }
 
+                @Override
+                public void onFailure(Throwable throwable) {
+                    Log.v(TAG, throwable.getMessage());
+                }
+            });
+        }else {
+            showToast("location null....");
         }
+    }
 
-        public void onStatusChanged(String provider, int status, Bundle extras) {
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        apiClient.connect();
+    }
+
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            providerClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    setLocationInfo(location);
+                }
+            });
+            apiClient.disconnect();
         }
+    }
 
-        public void onProviderEnabled(String provider) {
-        }
+    @Override
+    public void onConnectionSuspended(int i) {
+        showToast("onConnectionSuspended");
+    }
 
-        public void onProviderDisabled(String provider) {
-        }
-    };
-
-     */
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        showToast("onConnectionFailed");
+    }
 }
